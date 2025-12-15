2f65b32d70086d9570b59a53b236b41b8c86486d
Closer Official <info@closer-official.com>
2025-12-15 14:55:55 +0900

    firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,27 @@
+// Firebaseの機能をインポート（ブラウザ用CDN経由）
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } 
+    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, serverTimestamp } 
+    from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// あなたのFirebase設定
+const firebaseConfig = {
+  apiKey: "AIzaSyAmPN0z032Uo_ONsronn3smkIixRnBReJw",
+  authDomain: "closer-official.firebaseapp.com",
+  projectId: "closer-official",
+  storageBucket: "closer-official.firebasestorage.app",
+  messagingSenderId: "1039215613610",
+  appId: "1:1039215613610:web:540ef7a026714e28dbe2d3",
+  measurementId: "G-LYK0THB2Z2"
+};
+
+// アプリの初期化
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// 便利な機能を他のファイル（index.htmlなど）でも使えるように配る
+export { auth, db, analytics, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile, doc, setDoc, getDoc, serverTimestamp };
